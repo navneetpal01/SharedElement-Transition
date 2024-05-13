6c25680469e7cf8441d2f4729c1d0bd41b37b5fe
navneet <navneetpalgithub@gmail.com>
2024-05-13 11:24:46 +0530

    Hii from Marsyyy

---
+++ app/src/main/java/com/example/sharedelement_transition/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.sharedelement_transition
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.sharedelement_transition.ui.theme.SharedElementTransitionTheme
+
+
+class MainActivity : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
+        super.onCreate(savedInstanceState)
+        setContent {
+            SharedElementTransitionTheme {
+
+            }
+        }
+    }
+
+}
